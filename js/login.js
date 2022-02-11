93e7bd39596bf76bcd0064ec3694b726c2e9bb68
siddiqun03 <lenovoNoteb2ok@gmail.com>
2022-02-11 16:49:41 +0500

    finish: Log In.

---
+++ js/login.js
@@ -0,0 +1,54 @@
+// * Variables:
+const form = document.querySelector(".login-form");
+const userName = document.querySelector(".userName");
+const userPassword = document.querySelector(".password");
+const logInBtn = document.querySelector(".loginBtn");
+
+// * Evenetn delegation:
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  // * Variables:
+  const usernameValue = userName.value;
+  const passwordValue = userPassword.value;
+
+  // * Try/Catch:
+  try {
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: usernameValue,
+        password: passwordValue,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.token) {
+          // * Local storage:
+          window.localStorage.setItem("token", data.token);
+
+          window.location.replace("home.html");
+        } else {
+          // * Error style:
+          userName.addEventListener("keydown", (evt) => {
+            userName.style.borderColor = "#ffffff";
+            userName.style.color = "#ffffff";
+          });
+          userName.style.borderColor = "tomato";
+          userName.style.color = "tomato";
+          // * ---------------------------:
+          userPassword.addEventListener("keydown", () => {
+            userPassword.style.borderColor = "#ffffff";
+            userPassword.style.color = "#ffffff";
+          });
+          userPassword.style.borderColor = "tomato";
+          userPassword.style.color = "tomato";
+        }
+      });
+  } catch (error) {
+    alert("Sorry Something is wrong !");
+  }
+});
