606206cee35370580e0f10c47786e9597cc8d711
siddiqun03 <lenovoNoteb2ok@gmail.com>
2022-02-12 21:01:24 +0500

    add some func and fix bugs

--- js/home.js
+++ js/home.js
@@ -1,4 +1,6 @@
 // * Variables:
+const darkModeBtn = document.querySelector(".dark-mode-icon");
+const sortByNew = document.querySelector(".sortByNew");
 let elSearchInput = document.querySelector(".searchInput");
 const elTotalBook = document.querySelector(".total-book");
 let elBooksList = document.querySelector(".books-list");
@@ -17,11 +19,12 @@ const elPublishers = document.querySelector(".PublishersSpan");
 const elCategories = document.querySelector(".CategoriesSpan");
 const pages = document.querySelector(".Pages-CountSpan");
 let modalImg = document.querySelector(".modalImg");
+const modalRead = document.querySelector(".modal-read");
 // * -----------
 
 // * Bookmark:
 const elBookmarkList = document.querySelector(".bookmark-list");
-const bookmarkArr = [];
+let bookmarkArr = JSON.parse(window.localStorage.getItem("bookMark")) || [];
 // * ---------
 
 // * Log Out:
@@ -42,12 +45,21 @@ const getDate = (date) => new Date(date).getFullYear();
 
 // * async func:
 const getBooksApi = async function (book) {
-  let response = await fetch(API_KEY + book);
-  let date = await response.json();
-  elTotalBook.textContent = `Showing 10 Result(${date.totalItems || 0})`;
-  mainArr = date.items;
-  console.log(mainArr);
-  renderBooks(date.items, elBooksList);
+  try {
+    let response = await fetch(API_KEY + book);
+    let date = await response.json();
+    elTotalBook.textContent = `Showing ${date.items.length} Result(${date.totalItems})`;
+    elTotalBook.style.color = "green";
+    mainArr = date.items;
+    console.log(mainArr);
+    elBooksList.innerHTML= null
+    renderBooks(date.items, elBooksList);
+  } catch (error) {
+    console.log(error);
+    elTotalBook.textContent = `Kino topilmadi`;
+    elTotalBook.style.color = "red";
+    elBooksList.innerHTML = null;
+  }
 };
 // * Render function:
 const renderBooks = (arr, elemente) => {
@@ -59,7 +71,7 @@ const renderBooks = (arr, elemente) => {
     let booksItem = document.createElement("li");
 
     // * set atribute:
-    booksItem.setAttribute("class", "books-item");
+    booksItem.setAttribute("class", "books-item dark-input");
 
     // * equal inner html:
     booksItem.innerHTML = `<div class="img-box">
@@ -76,7 +88,7 @@ const renderBooks = (arr, elemente) => {
       <button class="bookmarkBtn" data-bookmark=${element.id}>Bookmark</button>
       <button class="moreInfoBtn" data-info=${element.id}>More info</button>
     </div>
-    <button class="book-read">Read</button>`;
+    <a href=${element.volumeInfo?.previewLink} class="book-read">Read</a>`;
 
     // * Append item to list:
     elemente.appendChild(booksItem);
@@ -86,8 +98,20 @@ const renderBooks = (arr, elemente) => {
 // * search on time:
 elSearchInput.addEventListener("keyup", () => {
   searchValue = elSearchInput.value;
-  getBooksApi(searchValue);
+  if (searchValue !== "") {
+    getBooksApi(searchValue);
+  }else{
+      elBooksList.innerHTML = null
+      elTotalBook.textContent = `Showing 0 Result(0)`;
+      elTotalBook.style.color = 'red'
+  }
 });
+
+// * Sort by new:
+sortByNew.addEventListener("click", (evt) => {
+  getBooksApi(searchValue.value + "&orderBy=newest");
+});
+
 // * Modal open:
 elBooksList.addEventListener("click", (evt) => {
   if (evt.target.matches(".moreInfoBtn")) {
@@ -100,37 +124,28 @@ elBooksList.addEventListener("click", (evt) => {
         elAuthor.textContent = element.volumeInfo.authors?.join(", ");
         elPublished.textContent = getDate(element.volumeInfo.publishedDate);
         elPublishers.textContent = element.volumeInfo.publisher;
-        elCategories.textContent = element.volumeInfo.categories.join(", ");
+        elCategories.textContent = element.volumeInfo.categories?.join(", ");
+        modalRead.setAttribute("href", element.volumeInfo?.previewLink);
+        pages.textContent = element.volumeInfo?.pageCount;
       }
     });
   }
 });
 
-// * Create bookmark:
-elBooksList.addEventListener("click", (evt) => {
-  if (evt.target.matches(".bookmarkBtn")) {
-    mainArr.forEach((element) => {
-      if (element.id == evt.target.dataset.bookmark) {
-        elBookmarkList.innerHTML = null;
-        bookmarkArr.push({
-          id: element.id,
-          title: element.volumeInfo.title,
-          author: element.volumeInfo.authors?.join(", "),
-        });
+// * Render bookmark func:
+const renderBookmark = (objBookmark) => {
+  elBookmarkList.innerHTML = null;
+  objBookmark.forEach((element) => {
+    // * Create item for bookmark:
+    let bookItem = document.createElement("li");
 
-        // * ------------
+    //  * set atribute book items:
+    bookItem.setAttribute("class", "bookmark-item dark-input");
 
-        bookmarkArr.forEach((element) => {
-          // * Create item for bookmark:
-          let bookItem = document.createElement("li");
+    //  * Append bookmark to bookmark list:
+    elBookmarkList.appendChild(bookItem);
 
-          //  * set atribute book items:
-          bookItem.setAttribute("class", "bookmark-item");
-
-          //  * Append bookmark to bookmark list:
-          elBookmarkList.appendChild(bookItem);
-
-          bookItem.innerHTML = `
+    bookItem.innerHTML = `
                   <div class="b-item-left">
                     <h3 class="item-heading">${(elModalHeader.textContent =
                       element.title)}</h3>
@@ -138,11 +153,51 @@ elBooksList.addEventListener("click", (evt) => {
                       element.author)}</p>
                   </div>
                   <div class="b-item-right">
-                    <button class="readBook"></button>
-                    <button class="removeBook"></button>
+                    <a class="readBook" href=${element.read}></a>
+                    <button class="removeBook" data-remove=${
+                      element.id
+                    }></button>
                   </div>`;
+  });
+};
+
+// * Create bookmark:
+elBooksList.addEventListener("click", (evt) => {
+  if (evt.target.matches(".bookmarkBtn")) {
+    mainArr.forEach((element) => {
+      if (element.id == evt.target.dataset.bookmark) {
+        bookmarkArr.push({
+          id: element.id,
+          title: element.volumeInfo.title,
+          author: element.volumeInfo.authors?.join(", "),
+          read: element.volumeInfo?.previewLink,
         });
+        renderBookmark(bookmarkArr);
+        window.localStorage.setItem("bookMark", JSON.stringify(bookmarkArr));
       }
     });
   }
 });
+
+// * Detlete bookmark:
+elBookmarkList.addEventListener("click", (evt) => {
+  if (evt.target.matches(".removeBook")) {
+    bookmarkArr.forEach((element) => {
+      if (element.id == evt.target.dataset.remove) {
+        let findIndex = bookmarkArr.findIndex((InElement) => {
+          return evt.target.dataset.remove == InElement.id;
+        });
+        bookmarkArr.splice(findIndex, 1);
+        window.localStorage.setItem("bookMark", JSON.stringify(bookmarkArr));
+        renderBookmark(bookmarkArr);
+      }
+    });
+  }
+});
+// * Dark mode:
+darkModeBtn.addEventListener("click", () =>
+  document.body.classList.toggle("dark")
+);
+renderBookmark(bookmarkArr);
+
+// * pagenation:
